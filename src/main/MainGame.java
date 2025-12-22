f8b7880458d73a13909a4e1edbd6c55a649fdd36
GRAAHHGH <johndavidjaca@gmail.com>
2025-12-22 12:38:03 +0800

    wowzers, i wil ladd more features next if I have time

--- src/main/MainGame.java
+++ src/main/MainGame.java
@@ -12,11 +12,11 @@ public class MainGame { // MainGame is the starting class that contains the main
     yn = key.next().charAt(0);
 
     if(yn == 'y' || yn == 'Y'){
-        typeMessage(secretfile.YES);
-        typeMessage(secretfile.jaryl);
-        typeMessage(secretfile.gerose);
-        typeMessage(secretfile.francis);
-        typeMessage(secretfile.jd);
+        // typeMessage(secretfile.YES); // after you done reading u can comment this all
+        // typeMessage(secretfile.jaryl);
+        // typeMessage(secretfile.gerose);
+        // typeMessage(secretfile.francis);
+        // typeMessage(secretfile.jd);
         new Game(); // to access contructor which is the game.java
     }
     else{
